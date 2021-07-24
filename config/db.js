22fa6aa9013be2fbe19bebcf177703066ba87b7e
Salko Balić <salkobalic3@gmail.com>
2021-07-24 17:48:03 +0200

    Env variables added

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-const DB_NAME = "ecommerce-mern";
-const DB_USER = "ecommerce-mern";
-const DB_USER_PW = "ZlrtLjl6x0cGKk8Y";
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_USER_PW = process.env.DB_USER_PW;
 
 const DB_URI = `mongodb+srv://${DB_USER}:${DB_USER_PW}@cluster0.prbks.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
